509cd687ffff7f1e6a86274aa5947b581843978e
Nguyen Duy Quang <quangnd.92@gmail.com>
2016-10-04 15:38:21 +0700

    update

---
+++ app/app-ctor.component.js
@@ -0,0 +1,10 @@
+"use strict";
+var AppCtorComponent = (function () {
+    function AppCtorComponent() {
+        this.title = 'Tour of Heroes';
+        this.myHero = 'Windstorm';
+    }
+    return AppCtorComponent;
+}());
+exports.AppCtorComponent = AppCtorComponent;
+//# sourceMappingURL=app-ctor.component.js.map
